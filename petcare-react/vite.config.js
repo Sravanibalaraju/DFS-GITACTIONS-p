48d2863794729c5797673485f8d431c97c11b983
Sravani <sravanishiva192@gmail.com>
2025-10-24 14:29:58 +0530

    hui

--- petcare-react/vite.config.js
+++ petcare-react/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // set your desired port
+    port: 3000, // set your desired port
   },
 });
